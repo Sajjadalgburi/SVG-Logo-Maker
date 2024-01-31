19431fe3d54c9b9e562c2965deea6122c4269b1d
Sajjadalgburi <dfgsajjad@gmail.com>
2024-01-31 13:14:52 -0700

    added logic to handle generating the  appropriate logo

---
+++ util/generatedLogoContent.js
@@ -0,0 +1,16 @@
+const { Shape, Circle, Triangle, Square } = require("../lib/shapes.js");
+
+function generatedLogoContent({ shape, shapeColor, textContent, textColor }) {
+  if (shape === "Circle") {
+    const circle = new Circle(shapeColor, textColor, textContent);
+    return circle.render();
+  } else if (shape === "Triangle") {
+    const triangle = new Triangle(shapeColor, textColor, textContent);
+    return triangle.render();
+  } else if (shape === "Square") {
+    const square = new Square(shapeColor, textColor, textContent);
+    return square.render();
+  }
+}
+
+module.exports = generatedLogoContent;
